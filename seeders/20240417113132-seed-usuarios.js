cb9ce1e82d0c5637ca26ec01271892cf37ae3d2d
Anahi-Nany <a324947@alumnos.uaslp.mx>
2024-05-22 14:21:56 -0600

    Eición de usarios default de colaborador y  Admin

--- seeders/20240417113132-seed-usuarios.js
+++ seeders/20240417113132-seed-usuarios.js
@@ -6,7 +6,7 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     const data = [];
 
-    for (let i = 2; i <= 21; i++) {
+    for (let i = 3; i <= 21; i++) {
       data.push({
         id_usuario: i,
         nombre: faker.name.firstName(),
@@ -53,22 +53,43 @@ module.exports = {
     }
 
     data.push({
+      id_usuario: 2,
+      nombre: 'Juan Antonio',
+      ap_paterno: 'Alvarado',
+      ap_materno: 'Sanchez',
+      telefono_fijo: faker.phone.phoneNumberFormat(),
+      telefono_celular: faker.phone.phoneNumberFormat(),
+      correo: 'colaborador@colaborador.com',
+      contraseña: 12345678,
+      especialidad: 'Medicina Interna',
+      sub_especialidad: 'Cardiología',
+      ultima_cedula_dgp:Math.floor(Math.random() * 9000000000) + 1000000000,
+      id_titulo: 1,
+      id_licenciatura: Math.floor(Math.random() * 10) + 1,
+      id_entidad: Math.floor(Math.random() * 20) + 1,
+      id_grado: Math.floor(Math.random() * 6) + 1,
+      fecha_nacimiento: faker.date.past(30, new Date('2002-01-01')),
+      fecha_registro: faker.date.past(30, new Date()),
+      id_rol: 2,
+    });
+
+    data.push({
       id_usuario: 1,
-      nombre: 'Administrador',
-      ap_paterno: faker.name.lastName(),
-      ap_materno: faker.name.lastName(),
+      nombre: 'Sandra E.',
+      ap_paterno: 'Guerra',
+      ap_materno: 'Palomares',
       telefono_fijo: faker.phone.phoneNumberFormat(),
       telefono_celular: faker.phone.phoneNumberFormat(),
       correo: 'admin@admin.com',
       contraseña: 12345678,
-      especialidad: faker.company.bsNoun(),
-      sub_especialidad: faker.company.bsBuzz(),
+      especialidad: 'Biomedicina',
+      sub_especialidad: 'Genómica Viral y Epidemiología Molecular',
       ultima_cedula_dgp:Math.floor(Math.random() * 9000000000) + 1000000000,
-      id_titulo: Math.floor(Math.random() * 12) + 1,
+      id_titulo: 1,
       id_licenciatura: Math.floor(Math.random() * 10) + 1,
       id_entidad: Math.floor(Math.random() * 20) + 1,
-      id_grado: Math.floor(Math.random() * 6) + 1,
-      fecha_nacimiento: faker.date.past(30, new Date('2000-01-01')),
+      id_grado: 5,
+      fecha_nacimiento: faker.date.past(30, new Date('1994-06-03')),
       fecha_registro: faker.date.past(30, new Date()),
       id_rol: 1,
     });
