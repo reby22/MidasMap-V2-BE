0c567889bd1e64da13df60eb03e9b65186cf8f67
Anahi-Nany <a324947@alumnos.uaslp.mx>
2024-09-18 16:35:39 -0600

    Configuration

---
+++ config/dbConnection.js
@@ -0,0 +1,18 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('midasmap', 'root', null, {
+    host: 'localhost',
+    port: 3306,
+    dialect: 'mysql'
+});
+
+const dbConnection = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Db connection has been establiched successfully');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+};
+
+module.exports = { dbConnection, sequelize }
